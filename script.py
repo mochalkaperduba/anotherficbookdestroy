71095cb19b0c228c21a0c2f04f95edbccbf91f5c
mochalkaperduba <125117054+mochalkaperduba@users.noreply.github.com>
2023-03-01 19:38:42 +0300

    Update script.py

--- script.py
+++ script.py
@@ -16,20 +16,24 @@ def loadarr(path):
   res += ']'
   return res
 
-chrome_options = Options()
-chrome_options.add_argument("--disable-web-security")
-d = chr.Chrome(version_main = 109, options = chrome_options)
-d.get("https://ficbook.net")
-
 f = open("1.js", "r", encoding='utf-8')
 js = f.read()
 f.close()
 
 file = open('logins.txt',mode='r',encoding="utf8")
-r = file.readlines()
+r = file.read().splitlines()
+file.close()
+
+acc = int(sys.argv[1])
+print(r[acc])
 
 js+='\n'
-js+="await spam(\""+r[int(sys.argv[1])].split(':')[0]+"\",\""+r[int(sys.argv[1])].split(':')[1]+"\","+loadarr('names.txt')+", "+loadarr('desc.txt')+", "+loadarr('to.txt')+", "+loadarr('stops.txt')+")"
+js+="await spam(\""+r[acc].split(':')[0]+"\",\""+r[acc].split(':')[1]+"\","+loadarr('names.txt')+", "+loadarr('desc.txt')+", "+loadarr('to.txt')+", "+loadarr('stops.txt')+")"
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-web-security")
+d = chr.Chrome(version_main = 109, options = chrome_options)
+d.get("https://ficbook.net")
 
 message_bytes = js.encode('utf-8')
 base64_bytes = base64.b64encode(message_bytes)
